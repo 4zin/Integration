1492e8adbbe7f6682af9c978e6d36607b8958121
Camilo Rodelo <camilo_jrl@hotmail.com>
2023-08-23 16:59:50 -0500

    Linkeo

--- rick_and_morty/src/components/Card.jsx
+++ rick_and_morty/src/components/Card.jsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 const Card = ({ id, name, species, gender, image, onClose }) => {
   return (
     <div>
-      <button onClick={() => onClose(id)}>X</button>
+      <Link>
+        <button onClick={() => onClose(id)}>X</button>
+      </Link>
       <h2>Name: {name}</h2>
       <h2>Species: {species} </h2>
       <h2>Gender: {gender}</h2>
